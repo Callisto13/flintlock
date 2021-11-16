9fadd47252f367236ffb6f66d527476fab8331b5
Claudia Beresford <claudiaberesford@gmail.com>
2021-11-16 16:24:03 +0000

    Alter skip dmsetup behaviour in e2e tool

--- test/tools/test/runner.py
+++ test/tools/test/runner.py
@@ -5,7 +5,6 @@ class Test:
     def __init__(self, auth_token, config):
         self.testCfg = config['test']
         devCfg = config['device']
-
         self.welder = Welder(auth_token, config)
         self.skip_teardown = self.testCfg['skip_teardown']
         self.dev_id = devCfg['id']
@@ -27,12 +26,12 @@ class Test:
         cmd = ['./test/e2e/test.sh',
                '-level.flintlockd', self.testCfg['flintlock_log_level'],
                '-level.containerd', self.testCfg['containerd_log_level'],
+               '-skip.setup.thinpool',
+               '-thinpool', 'flintlock-thinpool'
                ]
         if self.testCfg['skip_delete']:
             cmd.append('-skip.teardown')
             cmd.append('-skip.delete')
-        if self.testCfg['skip_dmsetup']:
-            cmd.append('-skip.setup.thinpool')
         try:
             self.welder.run_ssh_command(cmd, "/root/work/flintlock", False)
         except RuntimeError as e:
